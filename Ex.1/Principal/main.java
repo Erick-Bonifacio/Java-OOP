0686423048f7696dcacf0f775004cc5a27182055
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2023-11-23 10:18:58 -0300

    Adicao dos exercicios feitos

---
+++ Ex.1/Principal/main.java
@@ -0,0 +1,20 @@
+package Principal;
+
+import entities.Rectangle;
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+import java.util.Locale;
+
+public class main {
+    
+    
+    Rectangle rectangle;
+    Scanner sc = new Scanner(System.in);
+    Locale.s
+
+    System.out.println("digite dois numeros a e b:");
+    rectangle.altura = sc.nextDouble();
+    
+}
---
+++ Ex.1/entities/rectangle.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Rectangle {
+
+    public double altura;
+    public double largura;
+
+    public double area(){
+        return altura * largura;
+    }
+
+    public double perimetro(){
+        return altura + altura + largura + largura;
+    }
+
+    public double diagonal(){
+        return math.sqrt(math.pow(altura) + math.pow(largura));
+    }
+}
